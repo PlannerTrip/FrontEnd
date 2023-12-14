c8967ec117bfc22a065147e66f1190fd83d542b9
kanpech-t <kankasem3002@gmail.com>
2023-12-14 19:41:32 +0700

    setup typescript and add navigate

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Test from "./screens/Test";
+import Test2 from "./screens/Test2";
+
+type StackParamList = {
+  Test: undefined;
+  Test2: undefined;
+};
 
 export default function App() {
+  const Stack = createNativeStackNavigator<StackParamList>();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Test2">
+        <Stack.Screen name="Test" component={Test} />
+        <Stack.Screen name="Test2" component={Test2} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/Test.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { Button, Text, View } from "react-native";
+import type { NativeStackScreenProps } from "@react-navigation/native-stack";
+
+type StackParamList = {
+  Test: undefined;
+  Test2: undefined;
+};
+
+type Props = NativeStackScreenProps<StackParamList, "Test">;
+
+const Test = ({ navigation }: Props) => {
+  return (
+    <View>
+      <Text>test</Text>
+
+      <Button
+        title="go to test2"
+        onPress={() => navigation.navigate("Test2")}
+      />
+    </View>
+  );
+};
+
+export default Test;
