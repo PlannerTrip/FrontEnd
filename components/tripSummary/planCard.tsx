b659cc58f9d7975fe25edac6e309a75bbe74897a
kanpech-t <kankasem3002@gmail.com>
2024-03-10 21:02:28 +0700

    add timePicker

--- components/tripSummary/planCard.tsx
+++ components/tripSummary/planCard.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 
 import { Plan } from "../../interface/tripSummary";
 
@@ -9,11 +9,16 @@ import PlanPlaceCard from "./planPlaceCard";
 import { changeDateFormat } from "../../utils/function";
 
 import PlanActivityCard from "./planActivityCard";
-import DateTimePicker from "@react-native-community/datetimepicker";
+import DateTimePicker, {
+  DateTimePickerEvent,
+} from "@react-native-community/datetimepicker";
 
 import Empty from "../../assets/tripSummary/Empty.svg";
 
 const PlanCard = ({ dailyPlan }: { dailyPlan: Plan }) => {
+  const today = new Date();
+
+  // ===================== useState =====================
   const [plan, setPlan] = useState([
     ...dailyPlan.places.map((item) => ({
       id: item.placeId,
@@ -38,6 +43,50 @@ const PlanCard = ({ dailyPlan }: { dailyPlan: Plan }) => {
       longitude: 0,
     })),
   ]);
+
+  // ===================== function =====================
+  const handleChangeTime = (
+    type: string,
+    id: string,
+    event: DateTimePickerEvent,
+    date?: Date
+  ) => {
+    if (date) {
+      const hours = date.getHours().toString().padStart(2, "0");
+      const minutes = date.getMinutes().toString().padStart(2, "0");
+      const formattedTime = `${hours}:${minutes}`;
+      console.log(formattedTime);
+      setPlan((plan) =>
+        plan.map((dailyPlan) => {
+          if (dailyPlan.id === id) {
+            if (type === "startTime") {
+              return { ...dailyPlan, startTime: formattedTime };
+            } else if (type === "endTime") {
+              return { ...dailyPlan, endTime: formattedTime };
+            }
+          }
+          return dailyPlan;
+        })
+      );
+    }
+  };
+
+  const formatTime = (time: string) => {
+    const [hours, minutes] = time.split(":").map(Number);
+
+    const dateWithTime =
+      time === ""
+        ? new Date()
+        : new Date(
+            today.getFullYear(),
+            today.getMonth(),
+            today.getDate(),
+            hours,
+            minutes
+          );
+    return dateWithTime;
+  };
+
   return (
     <View className="w-[358px] p-[16px] bg-white mt-[16px]">
       {/* header */}
@@ -46,8 +95,6 @@ const PlanCard = ({ dailyPlan }: { dailyPlan: Plan }) => {
         <View className="ml-[8px]" />
         <TextTitle title={changeDateFormat(dailyPlan.date)} color="#FFC502" />
       </View>
-
-      <DateTimePicker value={new Date()} mode="time" textColor="#FFC502" />
 
       {/* content */}
       {plan.length === 0 ? (
@@ -61,10 +108,56 @@ const PlanCard = ({ dailyPlan }: { dailyPlan: Plan }) => {
           </Text>
         </View>
       ) : (
-        <>
+        <View>
           {plan.map((item) => {
             return (
               <View className="flex-row">
+                {/* time select */}
+                <View className="w-[40px] mr-[8px] h-[100px] opacity-1  mt-[16px] ">
+                  <Pressable onPress={() => {}}>
+                    <Text
+                      className={`${
+                        item.startTime === "" ? "text-[#D9D9D9]" : ""
+                      } absolute text-[12px] leading-[18px] font-bold`}
+                    >
+                      {item.startTime === "" ? "00:00" : item.startTime}
+                    </Text>
+                  </Pressable>
+                  <Text
+                    className={`top-[20px] absolute text-[12px] leading-[18px] font-bold ${
+                      item.startTime === "" || item.endTime === ""
+                        ? "text-[#D9D9D9]"
+                        : ""
+                    }`}
+                  >
+                    -
+                  </Text>
+                  <Text
+                    className={`${
+                      item.endTime === "" ? "text-[#D9D9D9]" : ""
+                    } absolute top-[40px] text-[12px] leading-[18px] font-bold`}
+                  >
+                    {item.endTime === "" ? "00:00" : item.endTime}
+                  </Text>
+                  <View className="opacity-0">
+                    <DateTimePicker
+                      value={formatTime(item.startTime)}
+                      mode="time"
+                      onChange={(event: DateTimePickerEvent, date?: Date) => {
+                        handleChangeTime("startTime", item.id, event, date);
+                      }}
+                    />
+
+                    <DateTimePicker
+                      value={formatTime(item.endTime)}
+                      mode="time"
+                      onChange={(event: DateTimePickerEvent, date?: Date) => {
+                        handleChangeTime("endTime", item.id, event, date);
+                      }}
+                    />
+                  </View>
+                </View>
+                {/* place and activity */}
                 {item?.type === "place" ? (
                   <PlanPlaceCard
                     key={item.id}
@@ -81,7 +174,7 @@ const PlanCard = ({ dailyPlan }: { dailyPlan: Plan }) => {
               </View>
             );
           })}
-        </>
+        </View>
       )}
     </View>
   );
