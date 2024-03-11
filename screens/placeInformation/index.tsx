1aefb9a8c19b9f10a149df5b4fb25ad2b910fbe6
Paranchai <paranchai.wt@gmail.com>
2024-03-11 15:48:16 +0700

    fix place information page

--- screens/placeInformation/index.tsx
+++ screens/placeInformation/index.tsx
@@ -1,23 +1,29 @@
-import React, { useCallback, useContext, useState } from "react";
+import React, { useCallback, useContext, useEffect, useState } from "react";
 import {
     View,
     Text,
-    Image,
     Pressable,
     Dimensions,
-    FlatList,
     ScrollView,
-    Alert,
-    Platform,
     TouchableOpacity,
 } from "react-native";
-import {
-    SafeAreaProvider,
-    useSafeAreaInsets,
-} from "react-native-safe-area-context";
+import { useFocusEffect } from "@react-navigation/native";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+
+import * as SecureStore from "expo-secure-store";
+
+import { API_URL } from "@env";
+
+import axios from "axios";
+
 import * as Linking from "expo-linking";
 
-// icon
+import { StackParamList } from "../../interface/navigate";
+
+import { AuthData } from "../../contexts/authContext";
+
+// =============== svg ===============
 import Arrow_left_brown from "../../assets/Arrow_left_brown.svg";
 import AddTrip from "../../assets/placeInformation/AddTrip.svg";
 import Bookmark from "../../assets/placeInformation/Bookmark.svg";
@@ -26,30 +32,46 @@ import Map from "../../assets/placeInformation/Map.svg";
 import Pin from "../../assets/placeInformation/Pin.svg";
 import TelephoneActive from "../../assets/placeInformation/TelephoneActive.svg";
 import WebsiteActive from "../../assets/placeInformation/WebsiteActive.svg";
-import Check from "../../assets/placeInformation/Check.svg";
-import CloseCircle from "../../assets/placeInformation/CloseCircle.svg";
+import Check from "../../assets/modal/checkCircle.svg";
+import Close from "../../assets/modal/closeCircle.svg";
 
-import * as SecureStore from "expo-secure-store";
-import { API_URL } from "@env";
-import axios from "axios";
-import { useFocusEffect } from "@react-navigation/native";
-
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { StackParamList } from "../../interface/navigate";
-
+// =============== components ===============
 import FlatListImage from "../../components/flatListImage";
 import Star from "../../components/placeInformation/star";
 import PercentageBar from "../../components/placeInformation/percentageBar";
 import ButtonCustom from "../../components/button";
 import Review from "../../components/placeInformation/review";
 import OfficeHours from "../../components/placeInformation/officeHours";
-import { AuthData } from "../../contexts/authContext";
-import { FAIL, LOADING, SUCCESS } from "../../utils/const";
 import ConfirmModal from "../../components/confirmModal";
 import Loading from "../Loading";
+
+// =============== utils ===============
+import { FAIL, LOADING, SUCCESS } from "../../utils/const";
 import { getCurrentLocation } from "../../utils/function";
 
 type Props = NativeStackScreenProps<StackParamList, "placeInformation">;
+
+const defaultData = {
+    placeName: "",
+    coverImg: [],
+    introduction: "",
+    location: {
+        address: "",
+        district: "",
+        province: "",
+    },
+    type: "",
+    tag: [],
+    weekDay: [],
+    totalCheckIn: 0,
+    // alreadyBookmark: false,
+    // alreadyCheckIn: false,
+    contact: { phone: "", url: "" },
+    forecasts: [],
+    latitude: 0,
+    longitude: 0,
+    placeId: "",
+};
 
 const PlaceInformation = ({ navigation, route }: Props) => {
     const insets = useSafeAreaInsets();
@@ -57,51 +79,18 @@ const PlaceInformation = ({ navigation, route }: Props) => {
     const { placeId, type, forecastDate, forecastDuration, from } =
         route.params;
 
-    const [data, setData] = useState({
-        placeName: "",
-        coverImg: [],
-        introduction: "",
-        location: {
-            address: "",
-            district: "",
-            province: "",
-        },
-        type: "",
-        tag: [],
-        weekDay: [],
-        review: {
-            alreadyLike: false,
-            content: "",
-            img: [[]],
-            profileUrl: "",
-            rating: 0,
-            reviewId: "",
-            totalLike: 0,
-            userId: "",
-            username: "",
-        },
-        totalCheckIn: 0,
-        alreadyBookmark: false,
-        alreadyCheckIn: false,
-        contact: { phone: "", url: "" },
-        forecasts: [],
-        latitude: 13.72042,
-        longitude: 100.528338,
-        placeId: "",
-    });
-
     const { width, height } = Dimensions.get("screen");
 
-    const { userId, token } = useContext(AuthData);
+    const { token } = useContext(AuthData);
 
-    const [numberOfReviews, setNumberOfReviews] = useState(0);
-    const [averageRating, setAverageRating] = useState(0);
-    const [ratingCounts, setRatingCounts] = useState<{ [key: number]: number }>(
-        {}
-    );
+    // =============== useState ===============
+    const [data, setData] = useState(defaultData);
+    const [bookmark, setBookmark] = useState(false);
+    const [checkIn, setCheckIn] = useState(false);
+
+    // display
     const [placeInformationStatus, setPlaceInformationStatus] =
         useState(LOADING);
-
     const [displayModalDeleteReview, setDisplayModalDeleteReview] =
         useState(false);
     const [displayModalBookmark, setDisplayModalBookmark] = useState(false);
@@ -109,14 +98,34 @@ const PlaceInformation = ({ navigation, route }: Props) => {
         useState(false);
     const [displayModalCheckInSuccess, setDisplayModalCheckInSuccess] =
         useState(false);
+    const [loadingButton, setLoadingButton] = useState(false);
 
+    // check-in
     const [permission, setPermission] = useState(true);
 
-    const [targetReviewId, setTargetReviewId] = useState("");
-
+    // icon
     const [disableTelephone, setDisableTelephone] = useState(true);
     const [disableWebsite, setDisableWebsite] = useState(true);
-    const [checkIn, setCheckIn] = useState(false);
+
+    // review
+    const [numberOfReviews, setNumberOfReviews] = useState(0);
+    const [averageRating, setAverageRating] = useState(0);
+    const [ratingCounts, setRatingCounts] = useState<{ [key: number]: number }>(
+        {}
+    );
+    const [targetReviewId, setTargetReviewId] = useState("");
+    const [deletedReview, setDeletedReview] = useState<string[]>([]);
+    const [reviews, setReviews] = useState({
+        alreadyLike: false,
+        content: "",
+        img: [[]],
+        profileUrl: "",
+        rating: 0,
+        reviewId: "",
+        totalLike: 0,
+        userId: "",
+        username: "",
+    });
 
     // =============== axios ===============
     const getPlaceInformation = async () => {
@@ -137,29 +146,12 @@ const PlaceInformation = ({ navigation, route }: Props) => {
             const getData = response.data;
             setData(getData);
 
-            const reviewsData = getData.review;
-
-            const numberOfReviews: number = reviewsData.length;
-            setNumberOfReviews(numberOfReviews);
-
-            const totalRating: number = reviewsData.reduce(
-                (sum, review) => sum + review.rating,
-                0
-            );
-            const averageRating: number =
-                numberOfReviews > 0 ? totalRating / numberOfReviews : 0;
-            setAverageRating(averageRating);
-
-            const ratingCounts: { [key: number]: number } = {};
-            reviewsData.forEach((review) => {
-                const rating = review.rating;
-                ratingCounts[rating] = (ratingCounts[rating] || 0) + 1;
-            });
-            setRatingCounts(ratingCounts);
+            handleReview(getData.review);
 
             setDisableTelephone(getData.contact.phone ? false : true);
             setDisableWebsite(getData.contact.url ? false : true);
 
+            setBookmark(getData.alreadyBookmark);
             setCheckIn(getData.alreadyCheckIn);
 
             setPlaceInformationStatus(SUCCESS);
@@ -181,11 +173,8 @@ const PlaceInformation = ({ navigation, route }: Props) => {
                     reviewId: reviewId,
                 },
             });
-            console.log("response.data", response.data);
-            if (response.data === "delete review success") {
-                setDisplayModalDeleteReview(false);
-            }
-            getPlaceInformation();
+
+            setDisplayModalDeleteReview(false);
         } catch (error) {
             if (axios.isAxiosError(error) && error.response) {
                 console.log(error.response.data);
@@ -204,10 +193,10 @@ const PlaceInformation = ({ navigation, route }: Props) => {
                     },
                 }
             );
-            if (!data.alreadyBookmark) {
+            if (!bookmark) {
                 setDisplayModalBookmark(true);
             }
-            getPlaceInformation();
+            setBookmark(!bookmark);
         } catch (error) {
             if (axios.isAxiosError(error) && error.response) {
                 console.log(error.response.data);
@@ -216,8 +205,9 @@ const PlaceInformation = ({ navigation, route }: Props) => {
     };
 
     const postCheckIn = async (latitude: number, longitude: number) => {
+        setLoadingButton(true);
         try {
-            const response = await axios.post(
+            await axios.post(
                 `${API_URL}/place/checkIn`,
                 {
                     latitude: latitude,
@@ -230,12 +220,14 @@ const PlaceInformation = ({ navigation, route }: Props) => {
                     },
                 }
             );
-            getPlaceInformation();
+            setCheckIn(true);
             setDisplayModalCheckInSuccess(true);
+            setLoadingButton(false);
         } catch (error) {
             if (axios.isAxiosError(error) && error.response) {
-                console.log(error.response.data);
                 console.log(error.response.data.error);
+                await new Promise((resolve) => setTimeout(resolve, 1000));
+                setLoadingButton(false);
                 setDisplayModalCheckInFail(true);
             }
         }
@@ -253,7 +245,7 @@ const PlaceInformation = ({ navigation, route }: Props) => {
         navigation.goBack();
     };
 
-    const handleReview = () => {
+    const handleGoReview = () => {
         navigation.navigate("review", {
             placeName: data.placeName,
             placeId: data.placeId,
@@ -266,8 +258,39 @@ const PlaceInformation = ({ navigation, route }: Props) => {
         setTargetReviewId(reviewId);
     };
 
-    const handleDeleteReviewConfirm = async () => {
+    const handleDeleteReviewConfirm = () => {
+        setDeletedReview((prev) => {
+            const updatedReview = [...prev];
+
+            if (!updatedReview.includes(targetReviewId)) {
+                updatedReview.push(targetReviewId);
+            }
+
+            return updatedReview;
+        });
+
         deleteReview(targetReviewId);
+    };
+
+    const handleReview = (reviewsData) => {
+        setReviews(reviewsData);
+        const numberOfReviews: number = reviewsData.length;
+        setNumberOfReviews(numberOfReviews);
+
+        const totalRating: number = reviewsData.reduce(
+            (sum, review) => sum + review.rating,
+            0
+        );
+        const averageRating: number =
+            numberOfReviews > 0 ? totalRating / numberOfReviews : 0;
+        setAverageRating(averageRating);
+
+        const ratingCounts: { [key: number]: number } = {};
+        reviewsData.forEach((review) => {
+            const rating = review.rating;
+            ratingCounts[rating] = (ratingCounts[rating] || 0) + 1;
+        });
+        setRatingCounts(ratingCounts);
     };
 
     // =============== function ===============
@@ -277,9 +300,6 @@ const PlaceInformation = ({ navigation, route }: Props) => {
         if (currentLocation) {
             const currentLatitude = currentLocation.coords.latitude;
             const currentLongitude = currentLocation.coords.longitude;
-            console.log(data.latitude, data.longitude);
-            console.log(currentLatitude, currentLongitude);
-
             postCheckIn(currentLatitude, currentLongitude);
         } else {
             setPermission(false);
@@ -293,7 +313,7 @@ const PlaceInformation = ({ navigation, route }: Props) => {
         return 0;
     };
 
-    //
+    // =============== icons ===============
     const handleTelephone = () => {
         if (!disableTelephone) {
             Linking.openURL(`tel:${data.contact.phone}`);
@@ -447,11 +467,7 @@ const PlaceInformation = ({ navigation, route }: Props) => {
                                 className="flex flex-col gap-[8px] items-center"
                                 onPress={handleBookmark}
                             >
-                                {data.alreadyBookmark ? (
-                                    <Bookmarked />
-                                ) : (
-                                    <Bookmark />
-                                )}
+                                {bookmark ? <Bookmarked /> : <Bookmark />}
 
                                 <Text className="text-[12px]">บุ๊กมาร์ก</Text>
                             </Pressable>
@@ -470,7 +486,7 @@ const PlaceInformation = ({ navigation, route }: Props) => {
                             <Text className="text-[16px] font-bold">
                                 จำนวนคนที่เคยมาเช็กอิน {data.totalCheckIn} คน
                             </Text>
-                            {data.alreadyCheckIn ? (
+                            {checkIn ? (
                                 <ButtonCustom
                                     title="เคยเช็กอินแล้ว"
                                     disable={true}
@@ -479,6 +495,7 @@ const PlaceInformation = ({ navigation, route }: Props) => {
                                 <ButtonCustom
                                     title="เช็กอิน"
                                     onPress={handleCheckIn}
+                                    loading={loadingButton}
                                 />
                             )}
                         </View>
@@ -543,33 +560,45 @@ const PlaceInformation = ({ navigation, route }: Props) => {
                                         fill="outline"
                                         size="small"
                                         width="w-[100px]"
-                                        onPress={handleReview}
+                                        onPress={handleGoReview}
                                     />
                                 </View>
                             </View>
 
                             <View className="mt-[16px] border-t-[0.5px] border-[#ABABB4]">
-                                {data.review &&
-                                    Array.isArray(data.review) &&
-                                    data.review.map((review, index) => {
-                                        return (
-                                            <Review
-                                                key={index}
-                                                username={review.username}
-                                                ownerId={review.userId}
-                                                profileUrl={review.profileUrl}
-                                                rating={review.rating}
-                                                createDate={review.createDate}
-                                                content={review.content}
-                                                img={review.img}
-                                                totalLike={review.totalLike}
-                                                alreadyLike={review.alreadyLike}
-                                                reviewId={review.reviewId}
-                                                handleDeleteReview={
-                                                    handleDeleteReview
-                                                }
-                                            />
-                                        );
+                                {reviews &&
+                                    Array.isArray(reviews) &&
+                                    reviews.map((review, index) => {
+                                        if (
+                                            !deletedReview.includes(
+                                                review.reviewId
+                                            )
+                                        ) {
+                                            return (
+                                                <Review
+                                                    key={index}
+                                                    username={review.username}
+                                                    ownerId={review.userId}
+                                                    profileUrl={
+                                                        review.profileUrl
+                                                    }
+                                                    rating={review.rating}
+                                                    createDate={
+                                                        review.createDate
+                                                    }
+                                                    content={review.content}
+                                                    img={review.img}
+                                                    totalLike={review.totalLike}
+                                                    alreadyLike={
+                                                        review.alreadyLike
+                                                    }
+                                                    reviewId={review.reviewId}
+                                                    handleDeleteReview={
+                                                        handleDeleteReview
+                                                    }
+                                                />
+                                            );
+                                        }
                                     })}
                             </View>
                         </View>
@@ -597,7 +626,7 @@ const PlaceInformation = ({ navigation, route }: Props) => {
                         <ConfirmModal
                             title={
                                 <View className="flex flex-col justify-center items-center">
-                                    <CloseCircle />
+                                    <Close />
                                     <Text className="mt-[8px] font-bold text-[16px] leading-[24px]">
                                         ไม่สามารถเช็คอินได้
                                     </Text>
@@ -610,7 +639,7 @@ const PlaceInformation = ({ navigation, route }: Props) => {
                                                 {data.placeName}
                                             </Text>
                                             <Text className="text-[12px] leading-[18px]">
-                                                ในระยะไม่เกิน 500 เมตร
+                                                ในระยะไม่เกิน 3 กิโลเมตร
                                             </Text>
                                         </>
                                     ) : (
